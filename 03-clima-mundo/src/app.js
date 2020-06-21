52bba132be6f888962d8c66450a1a31f1853a381
vakanal <alvaro.gordillo.reina@gmail.com>
2020-06-21 23:26:20 +0200

    New API Service

--- 03-clima-mundo/src/app.js
+++ 03-clima-mundo/src/app.js
@@ -1,3 +1,24 @@
+const axios = require("axios");
 const { argv } = require("./config");
 
-console.log(argv.direccion);
+const instance = axios.create({
+  baseURL: "https://community-open-weather-map.p.rapidapi.com",
+  headers: {
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+    "x-rapidapi-key": "a024f34a06msh43801dfe674ea2cp17ebe5jsn440714d7cf79",
+    useQueryString: true,
+    accept: "application/json",
+  },
+});
+
+const query = encodeURI(argv.direccion);
+
+instance
+  .get("/weather", {
+    params: {
+      q: query,
+    },
+  })
+  .then((resp) => console.log(resp.data))
+  .catch((err) => console.log(err));
