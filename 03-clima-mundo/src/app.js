59e2b3f7583bfeb09b6479a106899d41b71b168e
vakanal <alvaro.gordillo.reina@gmail.com>
2020-06-22 19:59:10 +0200

    Primera version del clima

--- 03-clima-mundo/src/app.js
+++ 03-clima-mundo/src/app.js
@@ -1,24 +1,6 @@
-const axios = require("axios");
 const { argv } = require("./config");
+const { getInfo } = require("./api");
 
-const instance = axios.create({
-  baseURL: "https://community-open-weather-map.p.rapidapi.com",
-  headers: {
-    "content-type": "application/octet-stream",
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-    "x-rapidapi-key": "a024f34a06msh43801dfe674ea2cp17ebe5jsn440714d7cf79",
-    useQueryString: true,
-    accept: "application/json",
-  },
-});
-
-const query = encodeURI(argv.direccion);
-
-instance
-  .get("/weather", {
-    params: {
-      q: query,
-    },
-  })
-  .then((resp) => console.log(resp.data))
-  .catch((err) => console.log(err));
+getInfo(argv.direccion)
+  .then((resp) => console.log(resp))
+  .catch((err) => console.error(err));
