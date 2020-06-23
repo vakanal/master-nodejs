245e85ac4fa3fedffa426b9f8faa862fa55ea603
gordil11 <alvaro.gordilloreina@aareon.com>
2020-06-23 17:32:17 +0200

    Previo a subida en heroku

---
+++ 04-webserver/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const hbs = require("hbs");
+require("./hbs/helpers");
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerPartials(`${__dirname}/views/partials`, (err) => {});
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "tomÁs dE aQUino",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
